2c2207466e426eb47f74f35e33d51eef13a5b02c
vernitapang <verni.tapang@gmail.com>
2024-10-22 19:26:24 +0800

    test case: login customer

--- cypress/e2e/specs/login.cy.js
+++ cypress/e2e/specs/login.cy.js
@@ -1,3 +1,4 @@
+const customer = require('../../fixtures/customer.json')
 describe('Login', () => {
     beforeEach(() => {
         cy.visit('/')
@@ -8,5 +9,12 @@ describe('Login', () => {
         cy.get(".mainHeading").should('be.visible', 'have.text', 'XYZ Bank')
         cy.get('.borderM > :nth-child(1) > .btn').should('be.visible', 'have.text', 'Customer Login')
         cy.get('.borderM > :nth-child(3) > .btn').should('be.visible', 'have.text', 'Bank Manager Login')
+    });
+
+    it('login as customer', () => {
+        cy.get('[ng-click="customer()"]').click()
+        cy.get('select').select(customer[1])
+        cy.get('form.ng-valid > .btn').click()
+        cy.get('.fontBig').should('have.text', customer[1])
     });
 });
