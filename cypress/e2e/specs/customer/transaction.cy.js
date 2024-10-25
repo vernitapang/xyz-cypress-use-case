ad1abb5c446fe6f883798a75987afce340b011c2
vernitapang <verni.tapang@gmail.com>
2024-10-25 18:21:45 +0800

    test case: headers & date time format

--- cypress/e2e/specs/customer/transaction.cy.js
+++ cypress/e2e/specs/customer/transaction.cy.js
@@ -22,4 +22,15 @@ describe('Customer Transactions', () => {
 
         cy.get('button[ng-click="scrollRight()"]').should('be.visible')
     });
+
+    it('verifies table headers', () => {
+        cy.get('thead > tr > :nth-child(1)').should('be.visible').and('include.text', 'Date-Time')
+        cy.get('thead > tr > :nth-child(2)').should('be.visible').and('include.text', 'Amount')
+        cy.get('thead > tr > :nth-child(3)').should('be.visible').and('include.text', 'Transaction Type')
+    });
+
+    it('verifies correct date & time format', () => {
+        cy.get('table > tbody > #anchor0 > :nth-child(1)').should('be.visible')
+            .invoke('text').should('match', /^[A-Z][a-z]{2} \d{1,2}, \d{4} \d{1,2}:\d{2}:\d{2} [AP]M$/)
+    });
 });
