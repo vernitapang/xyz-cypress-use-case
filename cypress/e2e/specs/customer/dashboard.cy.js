7eaefadbb03dd870d9009a0414c8dbd2f1a94ff4
Verni Tapang <verni.tapang@gmail.com>
2025-05-11 14:10:15 +0800

    refactor: added session storage

--- cypress/e2e/specs/customer/dashboard.cy.js
+++ cypress/e2e/specs/customer/dashboard.cy.js
@@ -1,37 +1,44 @@
-const customer = require('../../../fixtures/customer.json')
+const customer = require('../../../fixtures/currentUser.json');
+
 describe('Customer Dashboard', () => {
     const user = customer[1];
 
     beforeEach(() => {
-        cy.loginCustomer(user)
-    })
+        cy.window().then((win) => {
+            win.localStorage.setItem('CurrentUser', JSON.stringify(user));
+        });
+
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/account')
+    });
 
     it('verifies customer dashboard url', () => {
         cy.url().should('eq', 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/account')
     });
 
     it('verifies customer dashboard page elements', () => {
-        cy.get('.mainHeading').should('be.visible', 'have.text', 'XYZ Bank')
-        cy.get('.home').should('be.visible', 'have.text', 'Home')
-        cy.get('.logout').should('be.visible', 'have.text', 'Logout')
-        
-        cy.get('.borderM > :nth-child(1) > strong').should('be.visible', 'have.text', `Welcome ${user} !!`)
-        cy.get('#accountSelect').should('be.visible')
+        cy.get('.mainHeading').should('be.visible').and('have.text', 'XYZ Bank')
+        cy.get('.home').should('be.visible').and('have.text', 'Home')
+        cy.get('.logout').should('be.visible').and('have.text', 'Logout')
+
+        cy.get('.borderM > :nth-child(1) > strong')
+            .should('be.visible', 'have.text', `Welcome ${user.fName} ${user.lName} !!`)
+
+        cy.get('#accountSelect').should('be.visible');
         cy.get('.borderM > :nth-child(3)').should('be.visible')
             .and('include.text', 'Account Number')
             .and('include.text', 'Balance')
-            .and('include.text', 'Currency')
+            .and('include.text', 'Currency');
     });
 
-    it('verifies cta button for Transactions', () => {
+    it('verifies CTA button for Transactions', () => {
         cy.get('button[ng-click="transactions()"]').should('be.visible').should('include.text', 'Transactions')
     });
 
-    it('verifies cta button for Deposit', () => {
+    it('verifies CTA button for Deposit', () => {
         cy.get('button[ng-click="deposit()"]').should('be.visible').should('include.text', 'Deposit')
     });
 
-    it('verifies cta button for Withdrawal', () => {
+    it('verifies CTA button for Withdrawal', () => {
         cy.get('button[ng-click="withdrawl()"]').should('be.visible').should('include.text', 'Withdrawal')
     });
 
