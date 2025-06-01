56736de8b1687e7d45df3c8273b820da8012b728
Verni Tapang <verni.tapang@gmail.com>
2025-06-01 16:43:14 +0800

    config: mochareports

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  experimentalStudio: true,
   chromeWebSecurity: false,
   reporter: 'cypress-multi-reporters',
   reporterOptions: {
