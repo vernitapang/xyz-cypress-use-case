4e5786ea3c8822823e585a865a3951eca3d84744
Verni Tapang <verni.tapang@gmail.com>
2025-06-05 21:48:47 +0800

    test case: account opening

---
+++ cypress/e2e/specs/manager/openAccount.cy.js
@@ -0,0 +1,29 @@
+const managerActions = require('../../page-action/manager-actions')
+
+describe('Add Customer', () => {
+    beforeEach(() => {
+        cy.loginManager()
+        managerActions.clickOpenAccount()
+    });
+
+    it('verifies customer can open an account', () => {
+        cy.get('#userSelect').select('Hermoine Granger')
+        cy.get('#currency').select('Dollar')
+        cy.get('button[type="submit"]').click()
+        cy.on('window:alert', (alert) => {
+            expect(alert).to.eq('Account created successfully with account Number :1016')
+        });
+    });
+    
+    it('verifies empty form cannot be submitted', () => {
+        cy.get('button[type="submit"]').click()
+        cy.get('#userSelect').then(($input) => {
+            expect($input[0].checkValidity()).to.be.false;
+            expect($input[0].validationMessage).to.eq('Please select an item in the list.');
+        });
+        cy.get('#currency').then(($input) => {
+            expect($input[0].checkValidity()).to.be.false;
+            expect($input[0].validationMessage).to.eq('Please select an item in the list.');
+        });
+    });
+});
